a6e13629b89d8efd653730dddf912b2dd85d038b
Deedzy42 <44777267+Deedzy42@users.noreply.github.com>
2018-11-28 17:46:39 +0100

    Add files via upload

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amoachon <amoachon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/12 16:07:36 by amoachon          #+#    #+#             */
+/*   Updated: 2018/11/12 16:07:49 by amoachon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 char	*ft_strdup(char *src)
 {
-	int		i;
-	char	*str;
+	unsigned int	i;
+	char			*str;
 
 	i = 0;
 	while (src[i])
 		i++;
-	if (!(str = (char*)malloc(sizeof(str) * i)))
+	if (!(str = (char*)malloc(sizeof(str) * i + 1)))
 		return (0);
 	i = 0;
 	while (src[i])
